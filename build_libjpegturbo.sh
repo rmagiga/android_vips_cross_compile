86e310d3438b4b302f1cdfc94006d31a9e384a46
rmagiga <rharuakim-soft@yahoo.co.jp>
2025-10-05 19:11:52 +0900

    libjpeg-turboのビルドスクリプトを追加し、環境変数を設定

---
+++ build_libjpegturbo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+SCRIPTDIR=$(cd $(dirname $0); pwd)
+. $SCRIPTDIR/env.sh
+. $SCRIPTDIR/build_common.sh
+
+DOWNLOAD_FILE=${DOWNLOADDIR}/libjpeg-turbo-$LIBJPEG_TURBO_VERSION.tar.gz
+
+download https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/$LIBJPEG_TURBO_VERSION.tar.gz $DOWNLOAD_FILE
+extract $DOWNLOAD_FILE $SRCDIR/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
+
+cd $SRCDIR/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
+mkdir -p build && cd build
+
+cmake -G"Unix Makefiles" \
+  -DANDROID_ABI=arm64-v8a \
+  -DANDROID_ARM_MODE=arm \
+  -DANDROID_PLATFORM=android-${ANDROID_API} \
+  -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
+  -DCMAKE_ASM_FLAGS="--target=aarch64-linux-android${ANDROID_API}" \
+  -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  $SRCDIR/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
+
+make
+make install
