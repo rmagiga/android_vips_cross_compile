5b371710cb4b490456ad06625206cedf637ae88d
rmagiga <rharuakim-soft@yahoo.co.jp>
2025-10-10 23:52:10 +0900

    libjpeg-turboのビルドスクリプトを修正し、ダウンロードURLを変数化して可読性を向上。展開ディレクトリの指定を整理。

--- build_libjpegturbo.sh
+++ build_libjpegturbo.sh
@@ -4,23 +4,24 @@ SCRIPTDIR=$(cd $(dirname $0); pwd)
 . $SCRIPTDIR/env.sh
 . $SCRIPTDIR/build_common.sh
 
+URL="https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/$LIBJPEG_TURBO_VERSION.tar.gz"
 DOWNLOAD_FILE=${DOWNLOADDIR}/libjpeg-turbo-$LIBJPEG_TURBO_VERSION.tar.gz
+EXTRACT_DIR=$SRCDIR/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
 
-download https://github.com/libjpeg-turbo/libjpeg-turbo/archive/refs/tags/$LIBJPEG_TURBO_VERSION.tar.gz $DOWNLOAD_FILE
-extract $DOWNLOAD_FILE $SRCDIR/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
+download $URL $DOWNLOAD_FILE
+extract $DOWNLOAD_FILE $EXTRACT_DIR
 
-cd $SRCDIR/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
+cd $EXTRACT_DIR
 mkdir -p build && cd build
 
-cmake -G"Unix Makefiles" \
+cmake .. -G"Unix Makefiles" \
   -DANDROID_ABI=arm64-v8a \
   -DANDROID_ARM_MODE=arm \
   -DANDROID_PLATFORM=android-${ANDROID_API} \
   -DANDROID_TOOLCHAIN=${TOOLCHAIN} \
   -DCMAKE_ASM_FLAGS="--target=aarch64-linux-android${ANDROID_API}" \
   -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake \
-  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
-  $SRCDIR/libjpeg-turbo-$LIBJPEG_TURBO_VERSION
+  -DCMAKE_INSTALL_PREFIX=${PREFIX}
 
 make -j$(nproc)
 make install
