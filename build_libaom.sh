46610e945cd1681bfbd33734ae8bc271b9c05a12
rmagiga <rharuakim-soft@yahoo.co.jp>
2025-10-11 15:07:11 +0900

    build_libaom.shのダウンロードURLを変数化し、可読性を向上。展開ディレクトリの指定を整理。

--- build_libaom.sh
+++ build_libaom.sh
@@ -4,16 +4,19 @@ SCRIPTDIR=$(cd $(dirname $0); pwd)
 . $SCRIPTDIR/env.sh
 . $SCRIPTDIR/build_common.sh
 
+URL=https://storage.googleapis.com/aom-releases/libaom-$LIBAOM_VERSION.tar.gz
 DOWNLOAD_FILE=${DOWNLOADDIR}/aom-$LIBAOM_VERSION.tar.gz
-download https://storage.googleapis.com/aom-releases/libaom-$LIBAOM_VERSION.tar.gz $DOWNLOAD_FILE
-extract $DOWNLOAD_FILE $SRCDIR/aom-$LIBAOM_VERSION
+EXTRACT_DIR=$SRCDIR/aom-$LIBAOM_VERSION
 
-cd $SRCDIR/aom-$LIBAOM_VERSION
+download $URL $DOWNLOAD_FILE
+extract $DOWNLOAD_FILE $EXTRACT_DIR
+
+cd $EXTRACT_DIR
 mkdir build-android && cd build-android
 cmake .. -G"Unix Makefiles" \
   -DANDROID_ABI=arm64-v8a \
   -DANDROID_PLATFORM=android-${ANDROID_API} \
-  -DCMAKE_ASM_FLAGS="--target=aarch64-linux-android${ANDROID_API}" \
+  -DCMAKE_ASM_FLAGS="--target=${TARGET}${ANDROID_API}" \
   -DCMAKE_TOOLCHAIN_FILE=${ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake \
   -DCMAKE_INSTALL_PREFIX=${PREFIX} \
   -DENABLE_TESTS=0 \
